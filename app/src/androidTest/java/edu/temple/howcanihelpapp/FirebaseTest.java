964637c89cee37fb214b2b388d0857d7bf2917b9
jon-dez <48642222+jon-dez@users.noreply.github.com>
2022-04-05 02:50:36 -0400

    We can use the interfaces to do testing, without having to worry about Firebase not working IF for some reason it does not work. I highly doubt that we will encounter a problem though.

--- app/src/androidTest/java/edu/temple/howcanihelpapp/FirebaseTest.java
+++ app/src/androidTest/java/edu/temple/howcanihelpapp/FirebaseTest.java
@@ -5,19 +5,51 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import edu.temple.howcanihelpapp.Firebase.AuthenticationHelper;
+import edu.temple.howcanihelpapp.Firebase.AuthenticationHelperImpl;
 import edu.temple.howcanihelpapp.Firebase.User;
 
 public class FirebaseTest {
+    class AuthHelperImplTest implements AuthenticationHelper {
+
+        @Override
+        public boolean isAuthenticated() {
+            return false;
+        }
+
+        @Override
+        public void createUser(String email, String password, String name, String phoneNumber, AuthenticationHelperImpl.CreateUserHandler handler) throws Exception {
+
+        }
+    }
+
+    class UserImplTest implements User {
+
+        @Override
+        public String getDisplayName() {
+            return null;
+        }
+
+        @Override
+        public String getEmail() {
+            return null;
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            return null;
+        }
+    }
+
     @Test
     public void createUser() throws Exception {
-        AuthenticationHelper authHelper = new AuthenticationHelper();
+        AuthenticationHelperImpl authHelper = new AuthenticationHelperImpl();
         assertTrue("Expect no user to be authenticated before attempting to create a user.", !authHelper.isAuthenticated());
         authHelper.createUser(
                 "mendez.jonathan3099@gmail.com",
                 "test1234password",
                 "Jon",
                 "111111111",
-                new AuthenticationHelper.CreateUserHandler() {
+                new AuthenticationHelperImpl.CreateUserHandler() {
                     @Override
                     public void onCreateUserSuccess(User fibaUser) {
                         System.out.println("Success in creating a user!");
