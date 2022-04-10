02e7885abeb3c1115ee92b0c8f4720d378d5135e
jon-dez <48642222+jon-dez@users.noreply.github.com>
2022-04-09 21:30:53 -0400

    Refactor test

--- app/src/androidTest/java/edu/temple/howcanihelpapp/FirebaseTest.java
+++ app/src/androidTest/java/edu/temple/howcanihelpapp/FirebaseTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.*;
 
 import android.util.Log;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import edu.temple.howcanihelpapp.Firebase.AuthenticationHelper;
 import edu.temple.howcanihelpapp.Firebase.AuthenticationHelperImpl;
@@ -12,67 +16,89 @@ import edu.temple.howcanihelpapp.Firebase.CreateUserResult;
 import edu.temple.howcanihelpapp.Firebase.User;
 
 public class FirebaseTest {
+    static private int userNo = 0;
+
+    class UserI {
+        String displayName;
+        String email;
+        String password;
+        String phoneNumber;
+
+        UserI() {
+            displayName = String.format("user%d", userNo);
+            email = String.format("%s@email.test", displayName);
+            password = String.format("passwordFor%s", displayName);
+            phoneNumber = String.format("%10d", userNo).replace(' ', '0');
+            userNo++;
+        }
+
+        String getDisplayName() {
+            return displayName;
+        }
+
+        String getEmail() {
+            return email;
+        }
+
+        String getPassword() {
+            return password;
+        }
+
+        String getPhoneNumber() {
+            return phoneNumber;
+        }
+    }
+
+    @Before
+    public void resetAuth() throws ExecutionException, InterruptedException {
+        CompletableFuture<Boolean> signOutRes = new CompletableFuture<>();
+        AuthenticationHelper authenticationHelper = AuthenticationHelperImpl.getInstance();
+        authenticationHelper.signOut(new AuthenticationHelper.SignOutEventHandler() {
+            @Override
+            public void handle(boolean success) {
+                signOutRes.complete(success);
+            }
+        });
+
+        if(!signOutRes.get()) {
+            fail("Could not sign the user out!\nUser: " + authenticationHelper.getUser().toString());
+        }
+    }
 
     @Test
     public void createUser() throws Exception {
+        CompletableFuture<CreateUserResult> createRes = new CompletableFuture<>();
         AuthenticationHelper authHelper = AuthenticationHelperImpl.getInstance();
         assertTrue("Expect no user to be authenticated before attempting to create a user.", !authHelper.isAuthenticated());
+
+        UserI user = new UserI();
         authHelper.createUser(
-                "mendez.joodnatjaodhjane3099@gmail.com",
-                "test1234password",
-                "Jondo",
-                "111111111",
+                user.getEmail(),
+                user.getPassword(),
+                user.getDisplayName(),
+                user.getPhoneNumber(),
                 new AuthenticationHelperImpl.CreateUserHandler() {
                     @Override
                     public void handle(CreateUserResult res) {
-                        if(res.isSuccessful()) {
-                            User fibaUser = res.getCreatedUser();
-                            Log.d(
-                                    "createUser",
-                                    "Success in creating a user!" +
-                                    "\nName: " + fibaUser.getDisplayName() +
-                                    "\nEmail: " + fibaUser.getEmail() +
-                                    "\nPhoneNumber: " + fibaUser.getPhoneNumber() +
-                                    "\nUid: " + fibaUser.getUid()
-                            );
-                        } else {
-                            Log.w("createUser", "Unsuccessful in creating a user!");
-                            fail("Unsuccessful in creating a user!");
-                        }
+                        createRes.complete(res);
                     }
                 }
         );
-    }
 
-    @Test
-    public void getUserInfo() {
-        AuthenticationHelper authenticationHelper = AuthenticationHelperImpl.getInstance();
-        assertTrue("Expected an authenticated user before attempting to retrieve userinfo", authenticationHelper.isAuthenticated());
-        User fibaUser = authenticationHelper.getUser();
-        Log.d("getUserInfo: ", String.valueOf(fibaUser.getDisplayName().length()));
-        Log.d("getUserInfo",
-                "Success in getting userinfo" +
-                        "\nName: " + fibaUser.getDisplayName() +
-                        "\nEmail: " + fibaUser.getEmail() +
-                        "\nPhoneNumber: " + fibaUser.getPhoneNumber() +
-                        "\nUid: " + fibaUser.getUid()
-        );
-    }
-
-    @Test
-    public void signOutUser() {
-        AuthenticationHelper authHelper = AuthenticationHelperImpl.getInstance();
-        assertTrue("Expected an authenticated user before attempting a signOut", authHelper.isAuthenticated());
-        Log.d("User", authHelper.getUser().toString());
-        authHelper.signOut(new AuthenticationHelper.SignOutEventHandler() {
-            @Override
-            public void handle(boolean success) {
-                if(success) {
-                    assertFalse("Expected the user to not be authenticated.", authHelper.isAuthenticated());
-                    Log.d("signOutUser", "The user was signed out.");
-                } else
-                    assert(true);
-            }
-        });
+        CreateUserResult res = createRes.get();
+        if(res.isSuccessful()) {
+            User fibaUser = res.getCreatedUser();
+            Log.d(
+                    "createUser",
+                    "Success in creating a user!" +
+                            "\nName: " + fibaUser.getDisplayName() +
+                            "\nEmail: " + fibaUser.getEmail() +
+                            "\nPhoneNumber: " + fibaUser.getPhoneNumber() +
+                            "\nUid: " + fibaUser.getUid()
+            );
+        } else {
+            Log.w("createUser", "Unsuccessful in creating a user!");
+            fail("Unsuccessful in creating a user!");
+        }
     }
 }
