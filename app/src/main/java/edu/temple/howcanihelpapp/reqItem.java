a7ae42cf46fa8338a1d62f86a42fd97c7214ea3b
Hamsa Nandana Shaik <tun77242@temple.edu>
2022-04-10 21:46:59 -0400

    Wrote an activity for requesting items which opens from requests activity and created the layout for it

---
+++ app/src/main/java/edu/temple/howcanihelpapp/reqItem.java
@@ -0,0 +1,14 @@
+package edu.temple.howcanihelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reqItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_req_item);
+    }
+}
