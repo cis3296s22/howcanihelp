00068f3d206d91f0da129e9ea7642b55fcabd77a
Hamsa Nandana Shaik <tun77242@temple.edu>
2022-04-17 19:12:40 -0400

    Wrote code for the donations activity and modified the layout and also they can now launch from menu activity

--- app/src/main/java/edu/temple/howcanihelpapp/DonationsActivity.java
+++ app/src/main/java/edu/temple/howcanihelpapp/DonationsActivity.java
@@ -2,13 +2,55 @@ package edu.temple.howcanihelpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
 
 public class DonationsActivity extends AppCompatActivity {
+    Button searchReq, donItems, back;
+    GoogleMap mMap;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        DonationsActivity.this.finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donations);
+
+        searchReq = findViewById(R.id.searchReqBtn);
+        donItems = findViewById(R.id.donateItem);
+
+        //map
+        /*
+        SupportMapFragment smf = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView);
+        assert smf != null;
+        smf.getMapAsync((OnMapReadyCallback) this);
+        */
+
+        donItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DonationsActivity.this, RequestSearch.class);
+                startActivity(intent);
+            }
+        });
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DonationsActivity.this, MenuActivity.class);
+            }
+        });
+
     }
+
 }
--- app/src/main/java/edu/temple/howcanihelpapp/RequestSearch.java
+++ app/src/main/java/edu/temple/howcanihelpapp/RequestSearch.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class reqItem extends AppCompatActivity {
+public class RequestSearch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
