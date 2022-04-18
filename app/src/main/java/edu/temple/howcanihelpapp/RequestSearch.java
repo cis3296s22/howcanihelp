c10e9038bb0337294eeb32fa3484da549220b349
Hamsa Nandana Shaik <tun77242@temple.edu>
2022-04-18 13:12:46 -0400

    Modified layout and added more functions

--- app/src/main/java/edu/temple/howcanihelpapp/RequestSearch.java
+++ app/src/main/java/edu/temple/howcanihelpapp/RequestSearch.java
@@ -2,13 +2,36 @@ package edu.temple.howcanihelpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RequestSearch extends AppCompatActivity {
+
+    Button back, donate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_req_item);
+        setContentView(R.layout.activity_request_search);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RequestSearch.this, DonationsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        donate = findViewById(R.id.donate);
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RequestSearch.this, ConfirmPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
